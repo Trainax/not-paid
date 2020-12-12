fc181a35378c71d6e9e9821582b272985072e7e0
Davide Trainini <davide.trainax@gmail.com>
2020-12-12 10:22:02 +0100

    Codice migliore
    - Il codice è stato migliorato per renderlo più bello e funzionale
    - Alcune modifiche agli alert
    - Migliore impaginazione del README

--- README.md
+++ README.md
@@ -3,20 +3,24 @@
 Fai diventare il loro sito sempre più opaco fino a quando non svanisce completamente. Imposta una data di scadenza e personalizza il numero di giorni che gli offri per pagare fino a quando il sito web non è completamente svanito.
 
 ```javascript
-/* cambia queste variabili a tuo piacimento */
-var due_date = new Date('2019-12-31');
-var days_deadline = 60;
-/* non cambiare da qui in poi */
+/* Cambia queste variabili a tuo piacimento */
+
+var due_date = new Date('2030-12-31');
+var days_deadline = 30;
+
+/* Non cambiare da qui in poi */
 ```
 
 ## Uso
 
 Carica semplicemente il file not-paid.js nel `<head>`
 
--   È disponibile un plugin per WordPress [qui](https://github.com/SurfEdge/not-paid-wp).
--   C'è anche una versione per Android [qui](https://github.com/theapache64/faded).
--   C'è una versione per Windows Forms [qui](https://github.com/g-otn/winforms-not-paid).
+- È disponibile un plugin per WordPress [qui](https://github.com/SurfEdge/not-paid-wp)
+- C'è anche una versione per Android [qui](https://github.com/theapache64/faded)
+- C'è una versione per Windows Forms [qui](https://github.com/g-otn/winforms-not-paid)
 
-## Autore
+## Autori
 
-Inspired from twitter (@riklomas). Made by Ciprian (@kleampa), Romania. Modified and translated into Italian by Trainax.
+- Inspired from Twitter ([@riklomas](https://twitter.com/riklomas))
+- Made by Ciprian ([@kleampa](https://twitter.com/kleampa)), Romania
+- Modified and translated into Italian by Trainax ([@DavideTrainax](https://twitter.com/DavideTrainax))
--- not-paid.js
+++ not-paid.js
@@ -1,33 +1,36 @@
 (function() {
-  /* cambia queste variabili a tuo piacimento */
-  var due_date = new Date('2019-12-31');
-  var days_deadline = 60;
-  /* non cambiare da qui in poi */
+
+  /* Cambia queste variabili a tuo piacimento */
+
+  var due_date = new Date('2030-12-31'); //Data della scadenza per effettuare il pagamento. Formato YYYY-MM-DD
+  var days_deadline = 30; //Giorni di tolleranza dopo la scandenza. Non impostare un valore negativo qui, altrimenti il sito verrà visualizzato anche dopo il periodo di tolleranza!
+
+  /* Non cambiare da qui in poi */
+
+  const monthNames = ["gennaio", "febbraio", "marzo", "aprile", "maggio", "giugno", "luglio", "agosto", "settembre", "ottobre", "novembre", "dicembre"]
 
   var current_date = new Date();
   var utc1 = Date.UTC(due_date.getFullYear(), due_date.getMonth(), due_date.getDate());
   var utc2 = Date.UTC(current_date.getFullYear(), current_date.getMonth(), current_date.getDate());
-  var days = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
+  var days_late = Math.floor((utc2 - utc1) / (1000 * 60 * 60 * 24));
 
-  if (days > 0) {
-    var days_late = days_deadline - days;
-    var opacity = (days_late * 100 / days_deadline) / 100;
-    opacity = (opacity < 0) ? 0 : opacity;
-    opacity = (opacity > 1) ? 1 : opacity;
-    if (opacity >= 0 && opacity <= 1) {
-      document.getElementsByTagName("BODY")[0].style.opacity = opacity;
+  if (days_late > 0) {
+    var days_until_darkness = days_deadline - days_late;
+    var opacity = (days_until_darkness * 100 / days_deadline) / 100;
+    if (opacity < 0) {
+      opacity = 0;
+    } else if (opacity > 1) {
+      opacity = 1;
     }
 
-    if (opacity > 0 && opacity <= 1) {
-      if (days_late != 1) {
-        alert("Il termine per effettuare il pagamento del compenso per la realizzazione di questo sito web, il quale era fissato per il " + due_date.getDate() + "/" + (due_date.getMonth() + 1) + "/" + due_date.getFullYear() + ", è scaduto. Questo sito web diventerà sempre più opaco e tra poco non sarà più possibile vederlo. Si hanno ancora " + days_late + " giorni di tempo per poter effettuare il pagamento prima che questo sito web diventi totalmente illeggibile");
-      } else {
-        alert("Il termine per effettuare il pagamento del compenso per la realizzazione di questo sito web, il quale era fissato per il " + due_date.getDate() + "/" + (due_date.getMonth() + 1) + "/" + due_date.getFullYear() + ", è scaduto. Questo sito web diventerà sempre più opaco e tra un giorno non sarà più possibile vederlo. Si ha ancora un giorno di tempo per poter effettuare il pagamento prima che questo sito web diventi totalmente illeggibile");
-      }
-    }
+    document.getElementsByTagName("BODY")[0].style.opacity = opacity;
 
-    if (opacity == 0) {
-      alert("Tempo scaduto! Il pagamento non è stato effettuato nei tempi previsti ed ora il sito web non è più visibile");
+    if (days_until_darkness > 1) {
+      alert("Messaggio per il proprietario di questo sito web. Il termine per effettuare il pagamento del compenso per la realizzazione di questo sito web, il quale era fissato per il " + due_date.getDate() + " " + monthNames[due_date.getMonth()] + " " + due_date.getFullYear() + ", è scaduto. Questo sito web diventerà sempre più opaco e tra poco non sarà più possibile vederlo. Si hanno ancora " + days_until_darkness + " giorni di tempo per poter effettuare il pagamento prima che questo sito web diventi totalmente illeggibile");
+    } else if (days_until_darkness == 1) {
+      alert("Messaggio per il proprietario di questo sito web. Il termine per effettuare il pagamento del compenso per la realizzazione di questo sito web, il quale era fissato per il " + due_date.getDate() + " " + monthNames[due_date.getMonth()] + " " + due_date.getFullYear() + ", è scaduto. Questo sito web diventerà sempre più opaco e tra un giorno non sarà più possibile vederlo. Si ha ancora un giorno di tempo per poter effettuare il pagamento prima che questo sito web diventi totalmente illeggibile");
+    } else {
+      alert("Messaggio per il proprietario di questo sito web. Tempo scaduto! Il pagamento per la realizzazione di questo sito web non è stato effettuato nei tempi previsti ed ora il sito web non è più visibile. La scadenza era fissata per il " + due_date.getDate() + " " + monthNames[due_date.getMonth()] + " " + due_date.getFullYear() + " ed erano stati dati " + days_deadline + " giorni dopo tale data per poter saldare il pagamento");
     }
 
   }
